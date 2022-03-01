8709eaa715682ae4524f061c5780aec137531e6c
gubanovpm <petrgubanov37@gmail.com>
2022-03-01 17:22:08 +0300

    [sh0d4n]: trying to parse

--- calc.hs
+++ calc.hs
@@ -1,8 +1,11 @@
+import Data.Typeable
+
 -- main - функция, которая возвращает результат на экран
 main :: IO()
 main = do
     expression <- getLine
     printLA (lexer expression)
+    print (parse_expr (lexer expression))
 
 -- enum класс для перечисления возможных лексем
 data Lexem = ADD
@@ -12,10 +15,10 @@ data Lexem = ADD
            | LBR
            | RBR
            | NUM Int
-           | ERR            -- syntax error
+           | ERR                -- lexical error
            deriving (Eq, Show)
 
--- специальная функция вывода списка лексем
+-- printLA - специальная функция вывода списка лексем
 printLA :: [Lexem] -> IO()
 printLA = print
 
@@ -48,6 +51,7 @@ char2int char | (char == '0') =  0
               | (char == '9') =  9
               | otherwise     = -1
 
+-- isSpaces - проверка является ли символом - разделителем
 isSpaces :: Char -> Bool 
 isSpaces literal | (literal == ' ' ) = True
                  | (literal == '\t') = True
@@ -71,3 +75,51 @@ lexer_skip :: [Char] -> [Char]
 lexer_skip [] = []
 lexer_skip (x:xs) | (isFigure x) = lexer_skip xs
                   | otherwise = [x] ++ xs
+
+-- lexical_check - проверка корректности синтаксиса 
+lexical_check :: [Lexem] -> Bool
+lexical_check [] = True
+lexical_check (x:xs) | (x == ERR) = False
+                     | otherwise  = (lexical_check xs)
+
+-- parse_expr - смотри грамматику в прикрепленном комментарии
+parse_expr :: [Lexem] -> Double
+parse_expr []  = 1488
+parse_expr [x] = parse_term [x]
+parse_expr (x:s:xs) | (s == ADD) = (parse_mul [x]) + (parse_expr xs)
+                    | (s == SUB) = (parse_mul [x]) - (parse_expr xs)
+                    | otherwise  = parse_mul ([x] ++ xs)
+
+parse_mul :: [Lexem] -> Double
+parse_mul []  = 322
+parse_mul (x:xs) | (x == MUL) =  (parse_mul xs)
+                 | (x == DIV) = 1 / (parse_mul xs)
+                 | otherwise  = parse_term ([x] ++ xs)
+
+parse_term :: [Lexem] -> Double
+parse_term []  = 1337
+parse_term [x] = x :: Double
+parse_term (x:xs)   | (x == SUB) = - (parse_term xs)
+                    | otherwise  = parse_term ([x] ++ xs)
+
+-- cast :: (Typeable a, Typeable b) => a -> b
+-- cast x = if typeOf x == typeOf (undefined::String) then 1 else 2
+
+
+-- Trans :: Lexem -> Int
+-- Trans x | (x == NUM) = x
+--         | otherwise  = 0
+
+{- 
+grammar : 
+parse_expr = parse_mul + parse_expr
+           | parse_mul - parse_expr
+           | parse_mul
+parse_mul  = parse_term * parse_mul
+           | parse_term / parse_mul
+           | parse_term
+parse_term = (parse_expr)
+           | - parse_term
+           | num
+-}
+
