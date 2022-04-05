82a49e6e19db5289f46c7efba15be500e1244389
gubanovpm <petrgubanov37@gmail.com>
2022-04-05 18:05:02 +0300

    End with calculator

---
+++ Lexer.hs
@@ -0,0 +1,77 @@
+module Lexer
+    (
+      lexicallyAnalyse
+    )
+where
+import Data.Char (isDigit, isSpace)
+import Data.List (zip)
+import Data.Maybe
+import Tokens (Token, Token(..))
+
+data LexerState = Start          |
+                  Digits         |
+                  DigitsAfterDot |
+                  Operators      |
+                  Parens         |
+                  Error
+                  deriving (Show)
+
+isOperator :: Char -> Bool
+isOperator x = x `elem` ['+','-','*','/']
+
+isParens :: Char -> Bool
+isParens x = x `elem` ['(',')']
+
+lexicallyAnalyse :: String -> Maybe [Token]
+lexicallyAnalyse input = consume input Start [] []
+
+consume :: String -> LexerState -> [(LexerState, Char)] -> [Token] -> Maybe [Token]
+consume (x:xs) Start history tokens
+  | isDigit x    = consume xs Digits ((Start, x) : history) tokens
+  | isOperator x = consume xs Operators ((Start, x) : history) tokens
+  | isParens x   = consume xs Parens ((Parens, x) : history) tokens
+  | isSpace x    = consume xs Start history tokens
+  | otherwise    = Nothing
+
+consume [] Digits history tokens = consume [] Start [] (tokens ++ [Number number])
+  where number = read (historyAsStr history) :: Double
+consume input@(x:xs) Digits history tokens
+  | isDigit x = consume xs Digits ((Digits, x) : history) tokens
+  | isSpace x = consume xs Digits history tokens
+  | x == '.'  = consume xs DigitsAfterDot ((Digits, x) : history) tokens
+  | otherwise = consume input Start [] (tokens ++ [Number number])
+  where number = read (historyAsStr history) :: Double
+
+consume [] DigitsAfterDot history tokens = consume [] Start [] (tokens ++ [Number number])
+  where number = read (historyAsStr history) :: Double
+consume input@(x:xs) DigitsAfterDot history tokens
+  | isDigit x       = consume xs DigitsAfterDot ((DigitsAfterDot, x) : history) tokens
+  | isSpace x       = consume xs DigitsAfterDot history tokens
+  | prevChar == '.' = Nothing
+  | otherwise       = consume input Start [] (tokens ++ [Number number])
+  where (_, prevChar) = head history
+        number        = read (historyAsStr history) :: Double
+
+consume [] Operators history tokens = consume [] Start [] (tokens ++ [Operator operator])
+  where operator = (head . historyAsStr) history
+consume input@(x:xs) Operators history tokens
+  | isSpace x = consume xs Operators history tokens
+  | otherwise = consume input Start [] (tokens ++ [Operator operator])
+  where operator = (head . historyAsStr) history
+
+consume [] Parens history tokens = consume [] Start [] (tokens ++ [parensToken])
+  where parens      = (head . historyAsStr) history
+        parensToken = case parens of
+            '(' -> OpenParens
+            ')' -> CloseParens
+consume input Parens history tokens = consume input Start [] (tokens ++ [parensToken])
+  where parens      = (head . historyAsStr) history
+        parensToken = case parens of
+            '(' -> OpenParens
+            ')' -> CloseParens
+
+consume [] state history tokens = Just tokens
+consume _ _ _ _                 = Nothing
+
+historyAsStr :: [(LexerState, Char)] -> String
+historyAsStr history = reverse [ x | (state, x) <- history ]
---
+++ Main.hs
@@ -0,0 +1,28 @@
+module Main where
+import Lexer (lexicallyAnalyse)
+import Tokens (Token, Token(..))
+import Ast (
+  Exp, Exp(..),
+  Exp', Exp'(..),
+  Term, Term(..),
+  Term',Term'(..),
+  Factor, Factor(..),
+  Atom, Atom(..),
+  ArithOp, ArithOp(..))
+import Parser (parse)
+import Evaluator (evaluate)
+
+main :: IO ()
+main = do
+  expression <- getLine
+  maybeTokens <- return (lexicallyAnalyse expression)
+  print maybeTokens
+  case maybeTokens of
+    (Just tokens) -> do
+        maybeExp <- return (parse tokens)
+        case maybeExp of
+          (Just exp) -> do
+              result <- return (evaluate exp)
+              putStrLn (show result)
+          Nothing -> putStrLn "The input expression is not semantically valid!"
+    Nothing -> putStrLn "The input expression is not lexically valid!"
